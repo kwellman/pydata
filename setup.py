be587a4861f31b48cde91a2dbe534e83d01c3317
kwellman <none@none>
2011-03-07 17:34:25 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='datum',
+    version='0.1',
+    url='http://github.com/kwellman/datum',
+    author='Kenji Wellman',
+    description='Useful data like common names, words, stop words and more as python objects',
+    packages=['datum'],
+)
